d6e35aaa9e05135b0fe48bb1355b78385457bc2a
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2017-12-05 16:21:02 +0800

    bank页面

---
+++ src/main/java/com/bohai/finance/controller/BankController.java
@@ -0,0 +1,16 @@
+package com.bohai.finance.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class BankController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
