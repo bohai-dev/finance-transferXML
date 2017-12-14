15baea1198f1bdcb702d223ef0c2206fa28716f0
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2017-12-14 12:39:21 +0800

    自带jre

--- bin/start.bat
+++ bin/start.bat
@@ -10,4 +10,5 @@ setlocal enableextensions enabledelayedexpansion
 set classpath=.
 for %%c in (%CURRENT_DIR%\lib\*.jar) do set classpath=!classpath!;%%c
 
-start javaw -Dfile.encoding=UTF-8 -classpath %classpath% com.bohai.finance.main.Demo
+start jre\bin\javaw -Dfile.encoding=UTF-8 -classpath %classpath% com.bohai.finance.main.Demo
+
